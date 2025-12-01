bd37c79027e426c1ae767a35caec6ccc23d87411
Krishna Konda <kkchaitu@gmail.com>
2025-12-01 17:09:16 +0530

    km-bbb: target scripts to install new custom built images
    These scripts will flash u-boot or kernel images that have been
    built and copied to target using install scripts from the u-boot
    or kernel repositories.
    
    Signed-off-by: Krishna Konda <kkchaitu@gmail.com>

---
+++ target-scripts/km-bbb-install-bl-mmc.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "---------------------------------------------"
+if [ -z $1 ]
+then
+        echo "Command Usage: $0 </dev/mmcblkx>"
+        echo "x indicates 0(SDCARD) or 1(EMMC)"
+	echo "---------------------------------------------"
+        exit 0
+fi
+
+if [ $1 = "/dev/mmcblk1" ] || [ $1 = "/dev/mmcblk0" ] ; then
+	echo "Installing MLO image"
+        sudo dd if=MLO of=$1 count=2 seek=1 bs=128k
+	echo "Installing u-boot image"
+        sudo dd if=u-boot-dtb.img of=$1 count=4 seek=1 bs=384k
+        sync
+fi
+echo "---------------------------------------------"
---
+++ target-scripts/km-bbb-install-kernel-mmc.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+if [ -f uEnv.txt ]; then
+	KERNEL_UTS=$( grep uname_r  uEnv.txt | sed 's/uname_r=//g')
+	echo "---------------------------------------------"
+	echo "Found uname_r: $KERNEL_UTS"
+	echo "---------------------------------------------"
+	echo ""
+
+	sudo rm -rf  /boot/vmlinuz-4.19.94-Kernel-Masters-*
+	sudo rm -rf  /boot/config-4.19.94-Kernel-Masters-*
+	echo "copying vmlinux and config files"
+	echo "---------------------------------------------"
+	echo  "cp ${KERNEL_UTS}.zImage /boot/vmlinuz-${KERNEL_UTS}"
+	sudo cp ./${KERNEL_UTS}.zImage   /boot/vmlinuz-${KERNEL_UTS}
+	echo "cp ./config-${KERNEL_UTS}   /boot/"
+	sudo cp ./config-${KERNEL_UTS}   /boot/
+	echo ""
+
+	sudo rm -rf  /boot/dtbs/4.19.94-Kernel-Masters-*
+	#if [ -d "/boot/dtbs/${KERNEL_UTS}" ];then
+	#	echo "File is found"
+	#else
+	#	sudo mkdir -p  /boot/dtbs/${KERNEL_UTS}
+	#fi
+	#
+	#echo  "cp km-bbb-am335x.dtb /boot/dtbs/${KERNEL_UTS} "
+	#sudo cp km-bbb-am335x.dtb  /boot/dtbs/${KERNEL_UTS}/
+	sudo mkdir -p  /boot/dtbs/${KERNEL_UTS}
+	echo "extracting dtb and overlay files"
+	echo "---------------------------------------------"
+	echo  "tar -xf ./${KERNEL_UTS}-dtbs.tar.gz  -C /boot/dtbs/${KERNEL_UTS}/"
+	sudo tar -xf ./${KERNEL_UTS}-dtbs.tar.gz  -C  /boot/dtbs/${KERNEL_UTS}/
+
+	echo ""
+	echo "copying uEnv.txt"
+	echo "---------------------------------------------"
+	echo "sudo cp uEnv.txt /boot/uEnv.txt"
+	sudo cp uEnv.txt /boot/uEnv.txt
+	echo ""
+
+	sudo rm -rf  /usr/lib/modules/4.19.94-Kernel-Masters-*
+	echo "extracting kernel modules"
+	echo "---------------------------------------------"
+	echo  "tar -xf ./${KERNEL_UTS}-modules.tar.gz  -C /usr/"
+	sudo tar -xf ./${KERNEL_UTS}-modules.tar.gz  -C  /usr/
+	echo ""
+
+	echo "Removing zImage, dtb, modules and config"
+	echo "---------------------------------------------"
+	rm ./${KERNEL_UTS}.zImage
+	rm ./config-${KERNEL_UTS}
+	#rm am335x-boneblack.dtb
+	#rm km-bbb-am335x.dtb
+	rm ./${KERNEL_UTS}-dtbs.tar.gz
+	rm ./${KERNEL_UTS}-modules.tar.gz
+	rm ./uEnv.txt
+	echo ""
+else
+	echo "images not found. install images with SCP"
+fi
