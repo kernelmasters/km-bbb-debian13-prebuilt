90c1b131f65c30742806b52b0de14999e4385a84
Krishna Konda <kkchaitu@gmail.com>
2025-12-02 17:47:31 +0530

    km-bbb: fix u-boot image name
    Signed-off-by: Krishna Konda <kkchaitu@gmail.com>

--- target-scripts/km-bbb-install-bl-mmc.sh
+++ target-scripts/km-bbb-install-bl-mmc.sh
@@ -13,7 +13,7 @@ if [ $1 = "/dev/mmcblk1" ] || [ $1 = "/dev/mmcblk0" ] ; then
 	echo "Installing MLO image"
         sudo dd if=MLO of=$1 count=2 seek=1 bs=128k
 	echo "Installing u-boot image"
-        sudo dd if=u-boot-dtb.img of=$1 count=4 seek=1 bs=384k
+        sudo dd if=u-boot.img of=$1 count=4 seek=1 bs=384k
         sync
 fi
 echo "---------------------------------------------"
